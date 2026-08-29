2e4d2c275ec84b0ac1d4daf013a74725289cae60
Vix-design <anguel1993.88@gmail.com>
2026-08-28 18:37:32 -0600

    Primer commit - Semana 1

---
+++ Modulo_1/calculadora_imc.py
@@ -0,0 +1,21 @@
+# Datos personales
+
+# string: nombre
+nombre = "Victor Hugo"
+
+# string: apellido paterno
+apellido_paterno = "Garcia"
+
+# string: apellido materno
+apellido_materno = "Bejerano"
+
+# int: edad
+edad = 33
+
+# float: peso
+peso = 87.6
+
+# float: estatura
+estatura = 1.73
+
+print("Bienvenido a la calculadora de IMC")
---
+++ Modulo_1/variables.py
@@ -0,0 +1,19 @@
+nombre = "Victor"
+edad = 30
+print(nombre)
+print(edad)
+
+
+#Variable string/char (texto)
+saludo = 'Hola, Mundo!'
+#f-string
+print (f'Variables simples: (chocolate_caliente), (PI), (is_activel, (saludo)')
+#ーーーーーー
+#Variable list
+#lindex0, indexl, index2, ...]
+fruits = ['apple', 50, 'cherry', [1,2,3], True]
+#fruits [0] -- 'apple'
+
+#Variable Tuple
+# (value1, value2, ...)
+coordinates = (10, .0, 20.0)
