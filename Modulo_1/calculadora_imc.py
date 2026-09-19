28bd47e17b722dcc5b877f50eaf01effb96a9586
Vix-design <anguel1993.88@gmail.com>
2026-09-18 18:59:09 -0600

    Formatea salida y redondea IMC - Sem 3

--- Modulo_1/calculadora_imc.py
+++ Modulo_1/calculadora_imc.py
@@ -1,13 +1,13 @@
 # Datos personales
 
 # string: nombre
-nombre = input ("¿Cuál es tu nombre?")
+nombre = input ("¿Cuál es tu nombre?").capitalize()
 
 # string: apellido paterno
-apellido_paterno = input ("¿Cuál es tu apellido paterno?")
+apellido_paterno = input ("¿Cuál es tu apellido paterno?").capitalize()
 
 # string: apellido materno
-apellido_materno = input ("¿Cuál es tu apellido materno?")
+apellido_materno = input ("¿Cuál es tu apellido materno?").capitalize()
 
 # int: edad
 edad = int(input("¿Cuál es tu edad?"))
@@ -20,5 +20,4 @@ estatura = float(input("¿Cuál es tu estatura?"))
 
 imc = peso / estatura ** 2 
 
-print("Bienvenido a la calculadora de IMC")
-print("Tu IMC es:", imc)
+print(f"Nombre: {nombre} {apellido_paterno} {apellido_materno} | Edad: {edad} | Peso: {peso} kg | Estatura: {estatura} m | IMC: {imc:.2f}")
